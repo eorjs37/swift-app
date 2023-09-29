99afa6cd74981aaa5c57695792308dbdfac33807
macdaegeon <chleorjs37@gmail.com>
2023-09-29 13:00:22 +0900

    Initial Commit

---
+++ sampleapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  sampleapp
+//
+//  Created by 최대건 on 2023/09/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sampleapp/sampleappApp.swift
@@ -0,0 +1,17 @@
+//
+//  sampleappApp.swift
+//  sampleapp
+//
+//  Created by 최대건 on 2023/09/29.
+//
+
+import SwiftUI
+
+@main
+struct sampleappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
